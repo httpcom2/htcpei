4addcb190cd291be55face9c6a65ab3924d99ac3
httpcom2 <71306469+httpcom2@users.noreply.github.com>
2022-09-14 10:44:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ else
 fi
 
 uuid=$(cat /proc/sys/kernel/random/uuid)
-old_id="e55c8d17-2cf3-b21a-bcf1-eeacb011ed79"
+old_id="e55c8d77-2cf3-b21a-bcf7-eeacb011ed79"
 v2ray_server_config="/etc/v2ray/config.json"
 v2ray_client_config="/etc/v2ray/233blog_v2ray_config.json"
 backup="/etc/v2ray/233blog_v2ray_backup.conf"
