16b1858c23a06fb22dc58cd8abca0c35d6f65c4d
httpcom2 <71306469+httpcom2@users.noreply.github.com>
2022-09-14 10:48:28 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -56,7 +56,7 @@ if [[ $path_status ]]; then
 fi
 
 uuid=$(cat /proc/sys/kernel/random/uuid)
-old_id="e55c8d17-2cf3-b21a-bcf1-eeacb011ed79"
+old_id="e55c8d17-2cf3-b21a-bcf7-eeacb071ed79"
 v2ray_server_config="/etc/v2ray/config.json"
 v2ray_client_config="/etc/v2ray/233blog_v2ray_config.json"
 v2ray_pid=$(pgrep -f /usr/bin/v2ray/v2ray)
